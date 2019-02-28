83fbf00b06a979d91398f9687e4dd173a9630eaa
Ozeryanskyy <32719679+Ozeryanskyy@users.noreply.github.com>
2019-02-28 11:00:41 +0200

    Add files via upload

---
+++ Hello world/Hello world/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Hello world
+//
+//  Created by Anton Ozeryanskyy on 2/15/19.
+//  Copyright © 2019 Anton Ozeryanskyy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+}
+
---
+++ Hello world/Hello world/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Anton Ozeryanskyy on 2/15/19.
+//  Copyright © 2019 Anton Ozeryanskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
